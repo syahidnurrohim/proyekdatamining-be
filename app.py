596694257449e366dc4dcb793b22c71f57c49a2c
mrgb <porrosyhd123@MRGB-P725>
2024-05-13 16:19:38 +0700

    feat: update preprocessing

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, jsonify
+from PIL import Image
+from flask_cors import CORS
+import numpy as np
 import tensorflow as tf
 import gdown
 import os
 
 app = Flask(__name__)
+CORS(app, origins=["*"])
 
 
 @app.route("/health-check", methods=["GET"])
@@ -23,20 +27,42 @@ def downloadModel(fileId):
 # Define a route to predict
 @app.route("/predict", methods=["POST"])
 def predict():
-    model_path = './model.h5'
-    if os.path.exists(model_path) is False:
-        return jsonify({"status": "error", "message": "model does not exists"})
-    # Load the TensorFlow model
-    model = tf.keras.models.load_model(model_path)
+    try:
+        file = request.files.get('file')
+        if file is None:
+            return jsonify({"status": "error", "message": "file does not exists"})
+        
+        model_path = "./model.h5"
+        
+        if os.path.exists(model_path) is False:
+            return jsonify({"status": "error", "message": "model does not exists"})
+        
+        model = tf.keras.models.load_model(model_path, compile=False)
+        
+        image_open = Image.open(file)
+        image_open = image_open.resize((150, 150))
 
-    # Get the JSON data from the request
-    data = request.get_json()
+        image_array = tf.keras.utils.img_to_array(image_open)
+        image_array_resized = np.resize(image_array, (150, 150, 3))
+        
+        batch_image_array = np.array([image_array_resized])
+        arr = model.predict(batch_image_array)
+        # Define alphabet
+        alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-    # Perform inference
-    prediction = model.predict(data["input"])
+        # Convert probabilities to alphabet results
+        # Find index of maximum probability
+        max_index = np.argmax(arr)
 
-    # Return the prediction as JSON
-    return jsonify({"prediction": prediction.tolist()})
+        # Convert index to corresponding letter
+        result = alphabet[max_index]
+
+        return jsonify({"status": "ok", "data": {
+            "result": result
+        }})
+    except Exception as e:
+        print(e)
+        return jsonify({"status": "error", "message": str(e)})
 
 
 if __name__ == "__main__":
